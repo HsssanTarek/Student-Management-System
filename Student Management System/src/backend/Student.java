8fea234e5b12974f9ed1ff6e5405f032d926fbbe
fatymali007 <fatimali120507@gmail.com>
2025-10-24 17:47:23 -0700

    Student and StudentDatabase files_:)

--- Student Management System/src/backend/Student.java
+++ Student Management System/src/backend/Student.java
@@ -1,4 +1,73 @@
+
 package backend;
+import java.util.Arrays;
 
 public class Student {
+    private String fullName;
+    private String studentID;
+    private String department;
+    private String gender;
+    private int age;
+    private double gpa;
+    public static final String[] Deps={"computer" ,"mechanical","electrical","architectural","industrial"};
+    public static final String[] Genders={"female","male"};
+    public Student(String studentID, String fullName, String department ,String gender,int age,double gpa){
+        if (fullName == null || fullName.isEmpty()||fullName.matches(".*\\d.*"))
+            throw new IllegalArgumentException("Invalid Name");
+        if (studentID == null || studentID.isEmpty()||!studentID.matches("\\d{4,5}"))
+            throw new IllegalArgumentException("Invalid Student ID");
+        if (gender==null||gender.isEmpty()||!Arrays.asList(Genders).contains(gender.toLowerCase()))
+            throw new IllegalArgumentException("Invalid Gender");
+        if(department==null||department.isEmpty()||!Arrays.asList(Deps).contains(department.toLowerCase()))
+            throw new IllegalArgumentException("Invalid Department");
+        if( age <16)
+            throw new IllegalArgumentException("Invalid Age");
+        if (gpa<=0.0||gpa>4.0)
+            throw new IllegalArgumentException("Invalid GPA");
+        
+        this.fullName=fullName;
+        this.studentID=studentID;
+        this.department=department.toLowerCase();
+        this.gender=gender.toLowerCase();
+        this.age=age;
+        this.gpa=gpa;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public String getStudentID(){
+        return studentID;
+    }
+    public double getGPA(){
+        return gpa;
+    }
+    //before calling any setter make sure that inputs are trimmed!!
+    public void setAge(int age){
+        if( age <16)
+            throw new IllegalArgumentException("Invalid Age");
+        this.age=age;
+    }
+    public void setDepartment(String department){
+        if(department==null||department.isEmpty()||!Arrays.asList(Deps).contains(department.toLowerCase()))
+            throw new IllegalArgumentException("Invalid Department");
+        this.department=department;
+    }
+    public void setFullName(String fullName){
+          if (fullName == null || fullName.isEmpty()||fullName.matches(".*\\d.*"))
+            throw new IllegalArgumentException("Invalid Name");
+        this.fullName=fullName;
+    }
+    public void setGPA(double gpa){
+        if (gpa<=0.0||gpa>4.0)
+            throw new IllegalArgumentException("Invalid GPA");
+        this.gpa=gpa;
+    }
+
+    public String lineRepresentation(){
+        return this.studentID +','+ this.fullName +','+this.department+','+this.gender+','+this.age+','+this.gpa;
+    }
 }
--- Student Management System/src/backend/StudentDatabase.java
+++ Student Management System/src/backend/StudentDatabase.java
@@ -1,4 +1,106 @@
+
 package backend;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class StudentDatabase {
+    private ArrayList<Student> records = new ArrayList<>();
+    private String filename;
+
+    public StudentDatabase(String filename)
+    {
+        this.filename=filename;
+    }
+    public void readFromFile()throws Exception {
+        File f = new File(filename);
+        if (!f.exists()) {
+            throw new IOException("File not found: " + filename);
+        }
+        Scanner file = new Scanner(f);
+        while(file.hasNextLine())
+        {
+            String line = file.nextLine();
+            if(!line.isEmpty())
+            {
+                Student record=createRecordFrom(line);
+                if (record!=null)
+                    records.add(record);
+            }
+        }
+        file.close();
+
+    }
+    public Student createRecordFrom(String line){
+        String[] record = line.split(",");
+        if (record.length==6) {
+            String studentID = record[0].trim();
+            String fullName = record[1].trim();
+            String department = record[2].trim();
+            String gender=record[3].trim();
+            int age = Integer.parseInt(record[4].trim());
+            double gpa = Double.parseDouble(record[5].trim());
+            return new Student(studentID,fullName,department,gender, age, gpa);
+        }
+        else return null;
+    }
+    public ArrayList<Student> returnAllRecords(){
+        return records;
+    }
+    public boolean contains(String Id ){
+        for(Student record : records)
+        {
+            if (record.getStudentID().equals(Id)){
+        
+                return true;
+            }
+        }
+        return  false;
+    }
+    public Student  getRecord(String Id){
+        for(Student record : records)
+        {
+            if (record.getStudentID().equals(Id))
+            {
+                return record;
+            }
+        }
+        return null;
+    }
+    public void insertRecord(Student record) throws Exception {
+        if (contains(record.getStudentID()))
+            throw new Exception("Record already exists!");
+        records.add(record);
+    }
+    public void deleteRecord(String Id) throws Exception {
+        Student record = getRecord(Id);
+        if(record ==null)
+            throw new Exception("Record not found!");
+        records.remove(record);
+    }
+    public void updateRecord(Student bro)throws Exception{//MIGHT NEED TO CHANGE THIS ONEE
+        Student record =getRecord(bro.getStudentID());
+        if(record==null){
+            throw new Exception("Record does not exist!");
+        }
+        
+            record.setFullName(bro.getFullName());
+            record.setDepartment(bro.getDepartment());
+            record.setAge(bro.getAge());
+            record.setGPA(bro.getGPA());
+            }
+
+
+    public void saveToFile() throws IOException{
+        PrintWriter fw = new PrintWriter(new FileWriter(filename));
+        for (Student record : records)
+        {
+            fw.println(record.lineRepresentation());
+        }
+        fw.close();
+    }
 }

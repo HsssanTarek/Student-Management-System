03466cfb23229ddefd4c7d56033e50d1e1a3163d
yassin-124 <yassinelnobby@gmail.com>
2025-10-26 11:14:01 +0300

    student service and accesscontroller(Yassin)

--- Student Management System/src/backend/AccessController.java
+++ Student Management System/src/backend/AccessController.java
@@ -1,4 +1,25 @@
 package backend;
 
 public class AccessController {
+    private final String username="Admin";
+    private final String password="1234";
+
+    public AccessController(){
+
+    }
+
+    public AccessController(backend.AccessController a){
+        a.login(username,password);
+    }
+    public boolean login(String username,String password){
+        if(this.username==username && this.password== password)
+
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
--- Student Management System/src/backend/StudentService.java
+++ Student Management System/src/backend/StudentService.java
@@ -1,4 +1,37 @@
 package backend;
 
+import java.io.IOException;
+import java.sql.Struct;
+import java.util.ArrayList;
+
 public class StudentService {
+    private StudentDatabase database;
+
+    public StudentService()  throws Exception {
+        database = new StudentDatabase("Students.txt");
+        database.readFromFile();
+    }
+    public void addStudent(Student s) throws Exception {
+        database.insertRecord(s);
+        database.saveToFile();
+    }
+    public boolean removeStudent(String id) throws Exception {
+        database.deleteRecord(id);
+        database.saveToFile();
+        return true;
+    }
+    public Student findStudent(String id) {
+        return database.getRecord(id);
+    }
+    public ArrayList<Student> getAllStudents() {
+        return database.returnAllRecords();
+    }
+    public boolean updateStudent(Student s) throws Exception {
+        database.updateRecord(s);
+        database.saveToFile();
+        return true;
+    }
+    public void logout() throws IOException{
+        database.saveToFile();
+    }
 }

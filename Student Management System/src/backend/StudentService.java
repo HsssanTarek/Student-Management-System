9c069581be2f3002939816b7886d187678cf373b
Hassan Tarek <hassan.tarek.695@gmail.com>
2025-10-28 13:41:39 +0300

    Last Version
    Last Version

--- Student Management System/src/backend/StudentService.java
+++ Student Management System/src/backend/StudentService.java
@@ -9,25 +9,42 @@ public class StudentService {
         database = new StudentDatabase("Students.txt");
         database.readFromFile();
     }
-    public void addStudent(Student s) throws Exception {
+    public void addStudent( String fullName, String department ,String gender,int age,double gpa) throws Exception {
+        int studentID=generateNextID();
+        Student s= new Student(studentID,fullName, department ,gender,age,gpa);
         database.insertRecord(s);
         database.saveToFile();
     }
-    public void removeStudent(String id) throws Exception {
+    public void addStudent(int studentID, String fullName, String department ,String gender,int age,double gpa) throws Exception {
+        Student s= new Student(studentID,fullName, department ,gender,age,gpa);
+        database.insertRecord(s);
+        database.saveToFile();
+    }
+    public void removeStudent(int id) throws Exception {
         database.deleteRecord(id);
         database.saveToFile();
     }
-    public Student findStudent(String id) {
-        return database.getRecord(id);
-    }
+    public Student findStudent(String key) {
+        if (key.matches("\\d+")) {
+            int id = Integer.parseInt(key);
+           return database.getRecord(id);}
+        else
+            return database.getRecord(key);}
     public ArrayList<Student> getAllStudents() {
         return database.returnAllRecords();
     }
-    public void updateStudent(Student s) throws Exception {
+    public void updateStudent(int studentID, String fullName, String department ,String gender,int age,double gpa)throws Exception {
+        Student s= new Student(studentID,fullName, department ,gender,age,gpa);
         database.updateRecord(s);
         database.saveToFile();
     }
     public void logout() throws IOException{
         database.saveToFile();
+    }
+    private int generateNextID() {
+        ArrayList<Student> students = getAllStudents();
+        if (students.isEmpty()) return 1000;
+        int lastID = students.get(students.size() - 1).getStudentID();
+        return lastID + 1;
     }
 }
